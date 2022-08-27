e13b798701918a16144908b687be804746429f41
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-08-27 14:29:24 -0300

    primeiro commit

---
+++ sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/SistemaControleGuardaObjetosApplication.java
@@ -0,0 +1,13 @@
+package com.guarda_objetos.sistema_controle_guarda_objetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaControleGuardaObjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaControleGuardaObjetosApplication.class, args);
+	}
+
+}
---
+++ sistema_controle_guarda_objetos/src/test/java/com/guarda_objetos/sistema_controle_guarda_objetos/SistemaControleGuardaObjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guarda_objetos.sistema_controle_guarda_objetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaControleGuardaObjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
