9b0ec06fbb486daec6feab5bd3e0fd36d4284f3e
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-08-28 16:40:43 -0300

    Criação Dto.

---
+++ sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/dto/ClientesDto.java
@@ -0,0 +1,80 @@
+package com.guarda_objetos.sistema_controle_guarda_objetos.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+public class ClientesDto {
+
+    @NotBlank
+    private String cpfCliente;
+    @NotBlank
+    private String nome;
+    @NotBlank
+    private Date dataNascimento;
+
+    private String email;
+    @NotBlank
+    private String telefone;
+
+    private String telefoneFixo;
+
+    private String whatsapp;
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getTelefoneFixo() {
+        return telefoneFixo;
+    }
+
+    public void setTelefoneFixo(String telefoneFixo) {
+        this.telefoneFixo = telefoneFixo;
+    }
+
+    public String getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setWhatsapp(String whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+}
