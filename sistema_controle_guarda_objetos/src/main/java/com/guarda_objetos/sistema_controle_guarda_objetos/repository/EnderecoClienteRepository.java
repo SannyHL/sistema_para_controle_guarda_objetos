a2bd48a023c762d94db282d6bf9f73ecbe9f53fa
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-08-28 17:58:26 -0300

    Adicionada a busca por cpf e nome.

--- sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/repository/EnderecoClienteRepository.java
+++ sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/repository/EnderecoClienteRepository.java
@@ -2,6 +2,13 @@ package com.guarda_objetos.sistema_controle_guarda_objetos.repository;
 
 import com.guarda_objetos.sistema_controle_guarda_objetos.model.EnderecoCliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface EnderecoClienteRepository extends JpaRepository<EnderecoCliente, String> {
+
+    EnderecoCliente findByCpf(String cpfCliente);
+    List<EnderecoCliente> queryByNomeLike(String nome);
 }
