07fc23a2e5682746650ec5a99ddba586872c49fd
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-08-28 18:39:05 -0300

    Service atualizado.

--- sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/service/ClientesService.java
+++ sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/service/ClientesService.java
@@ -1,4 +1,28 @@
 package com.guarda_objetos.sistema_controle_guarda_objetos.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import com.guarda_objetos.sistema_controle_guarda_objetos.dto.ClientesDto;
+import com.guarda_objetos.sistema_controle_guarda_objetos.model.Clientes;
+import com.guarda_objetos.sistema_controle_guarda_objetos.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
 public class ClientesService {
+
+    @Autowired
+    private ClientesRepository clientesRepository;
+
+    public List<Clientes> getAll(){
+        return clientesRepository.findAll();
+
+    }
+
+
+
 }
--- sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/service/EnderecoClienteService.java
+++ sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/service/EnderecoClienteService.java
@@ -1,4 +1,12 @@
 package com.guarda_objetos.sistema_controle_guarda_objetos.service;
 
+import com.guarda_objetos.sistema_controle_guarda_objetos.repository.EnderecoClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EnderecoClienteService {
+
+    @Autowired
+    private EnderecoClienteRepository enderecoClienteRepository;
 }
