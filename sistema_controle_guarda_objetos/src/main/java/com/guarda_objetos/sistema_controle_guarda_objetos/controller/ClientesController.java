bba46b06588fcacfadd194d44fc56602e6a011a9
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-08-28 18:40:39 -0300

    Adicionados Beans de Spring.

--- sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/controller/ClientesController.java
+++ sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/controller/ClientesController.java
@@ -1,4 +1,12 @@
 package com.guarda_objetos.sistema_controle_guarda_objetos.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+@RequestMapping("/clientes")
 public class ClientesController {
 }
--- sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/controller/ObjetoArmazenadoController.java
+++ sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/controller/ObjetoArmazenadoController.java
@@ -1,4 +1,20 @@
 package com.guarda_objetos.sistema_controle_guarda_objetos.controller;
 
+import com.guarda_objetos.sistema_controle_guarda_objetos.service.ObjetoArmazenadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+@RequestMapping("/objetos")
 public class ObjetoArmazenadoController {
+
+    @Autowired
+    private ObjetoArmazenadoService objetoArmazenadoService;
+
+
 }
