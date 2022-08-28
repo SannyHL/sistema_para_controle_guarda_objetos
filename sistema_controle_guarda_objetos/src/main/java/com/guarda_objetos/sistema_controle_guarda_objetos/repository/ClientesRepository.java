44007108c125ee978368116f442d132f00cb68d2
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-08-28 18:04:23 -0300

    Adicionada a busca por cpf e nome.

--- sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/repository/ClientesRepository.java
+++ sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/repository/ClientesRepository.java
@@ -3,5 +3,11 @@ package com.guarda_objetos.sistema_controle_guarda_objetos.repository;
 import com.guarda_objetos.sistema_controle_guarda_objetos.model.Clientes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClientesRepository extends JpaRepository<Clientes, String>{
+
+    Clientes findByCpf(String cpfCliente);
+    List<Clientes> queryByNameLike(String nome);
+
 }
--- sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/repository/ObjetoArmazenadoRepository.java
+++ sistema_controle_guarda_objetos/src/main/java/com/guarda_objetos/sistema_controle_guarda_objetos/repository/ObjetoArmazenadoRepository.java
@@ -3,5 +3,11 @@ package com.guarda_objetos.sistema_controle_guarda_objetos.repository;
 import com.guarda_objetos.sistema_controle_guarda_objetos.model.ObjetoArmazenado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ObjetoArmazenadoRepository extends JpaRepository<ObjetoArmazenado, String> {
+
+    ObjetoArmazenado findByCpf(String cpfCliente);
+    List<ObjetoArmazenado> queryByNameLike(String nome);
+
 }
